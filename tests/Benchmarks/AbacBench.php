94371861cd941ba34118def36ae6589b249ba1b8
Arbi Kullakshi <arbikullakshi0@gmail.com>
2024-12-16 21:58:50 +0100

    Add benchmarking setup and improve AbacBench structure
    Introduce phpbench configuration and bootstrap file for benchmarks. Update AbacBench to extend TestCase and refactor methods for cleaner initialization. Simplify policies and improve maintainability of benchmarking logic.

--- tests/Benchmarks/AbacBench.php
+++ tests/Benchmarks/AbacBench.php
@@ -2,6 +2,7 @@
 
 namespace zennit\ABAC\Tests\Benchmarks;
 
+use Orchestra\Testbench\TestCase;
 use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
@@ -9,17 +10,16 @@ use Psr\SimpleCache\InvalidArgumentException;
 use stdClass;
 use zennit\ABAC\DTO\AccessContext;
 use zennit\ABAC\Enums\PermissionOperations;
-use zennit\ABAC\Enums\PolicyOperators;
 use zennit\ABAC\Exceptions\UnsupportedOperatorException;
 use zennit\ABAC\Exceptions\ValidationException;
-use zennit\ABAC\Models\UserAttribute;
+use zennit\ABAC\Providers\AbacServiceProvider;
 use zennit\ABAC\Services\AbacService;
 use zennit\ABAC\Tests\TestHelpers\PolicyBuilder;
 
 /**
  * @BeforeMethods({"setUp"})
  */
-class AbacBench
+class AbacBench extends TestCase
 {
     use PolicyBuilder;
 
@@ -27,114 +27,40 @@ class AbacBench
 
     private stdClass $subject;
 
-    public function setUp(): void
+    /**
+     * @Revs(1000)
+     *
+     * @Iterations(5)
+     *
+     * @throws UnsupportedOperatorException
+     * @throws ValidationException
+     * @throws InvalidArgumentException
+     */
+    public function benchEvaluateSimplePolicy(): void
     {
-        // Setup test environment
+        $context = new AccessContext(
+            $this->subject,
+            'posts',
+            PermissionOperations::INDEX->value
+        );
+
+        $this->abacService->evaluate($context);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
         $this->abacService = app(AbacService::class);
 
         // Create test subject
         $this->subject = new stdClass();
         $this->subject->id = 1;
-
-        UserAttribute::create([
-            'subject_type' => get_class($this->subject),
-            'subject_id' => $this->subject->id,
-            'attribute_name' => 'role',
-            'attribute_value' => 'user',
-        ]);
     }
 
-    /**
-     * @Revs(1000)
-     *
-     * @Iterations(5)
-     *
-     * @throws UnsupportedOperatorException
-     * @throws InvalidArgumentException
-     * @throws ValidationException
-     */
-    public function benchSimplePolicy(): void
+    protected function getPackageProviders($app): array
     {
-        $this->createPolicy('posts', PermissionOperations::INDEX->value, [
-            [
-                'operator' => PolicyOperators::EQUALS->value,
-                'attributes' => [['attribute_name' => 'role', 'attribute_value' => 'user']],
-            ],
-        ]);
-
-        $context = new AccessContext(
-            subject: $this->subject,
-            resource: 'posts',
-            operation: PermissionOperations::INDEX->value
-        );
-
-        $this->abacService->evaluate($context);
-    }
-
-    /**
-     * @Revs(1000)
-     *
-     * @Iterations(5)
-     *
-     * @throws InvalidArgumentException
-     * @throws UnsupportedOperatorException
-     * @throws ValidationException
-     */
-    public function benchComplexPolicy(): void
-    {
-        $this->createPolicy('posts', PermissionOperations::UPDATE->value, [
-            [
-                'operator' => PolicyOperators::AND->value,
-                'attributes' => [
-                    ['attribute_name' => 'role', 'attribute_value' => 'user'],
-                    ['attribute_name' => 'department', 'attribute_value' => 'IT'],
-                ],
-            ],
-            [
-                'operator' => PolicyOperators::OR->value,
-                'attributes' => [
-                    ['attribute_name' => 'is_owner', 'attribute_value' => 'true'],
-                    ['attribute_name' => 'is_moderator', 'attribute_value' => 'true'],
-                ],
-            ],
-        ]);
-
-        $context = new AccessContext(
-            subject: $this->subject,
-            resource: 'posts',
-            operation: PermissionOperations::UPDATE->value
-        );
-
-        $this->abacService->evaluate($context);
-    }
-
-    /**
-     * @Revs(100)
-     *
-     * @Iterations(5)
-     *
-     * @throws InvalidArgumentException
-     * @throws UnsupportedOperatorException
-     * @throws ValidationException
-     */
-    public function benchMultiplePolicies(): void
-    {
-        // Create 100 policies
-        for ($i = 0; $i < 100; $i++) {
-            $this->createPolicy("resource{$i}", PermissionOperations::INDEX->value, [
-                [
-                    'operator' => PolicyOperators::EQUALS,
-                    'attributes' => [['attribute_name' => 'role', 'attribute_value' => 'user']],
-                ],
-            ]);
-        }
-
-        $context = new AccessContext(
-            subject: $this->subject,
-            resource: 'resource50',
-            operation: PermissionOperations::INDEX->value
-        );
-
-        $this->abacService->evaluate($context);
+        return [
+            AbacServiceProvider::class,
+        ];
     }
 }
